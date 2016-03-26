8d1a30ad46a87d05f83162b9d3952503b7cad20a
AnghelLeonard <leoprivacy@yahoo.com>
2016-03-26 08:18:28 +0200

    Explicitly open/close a web socket channel

---
+++ JSF23OpenCloseWebSocket/src/main/java/beans/PushBean.java
@@ -0,0 +1,32 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.push.Push;
+import javax.faces.push.PushContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@ApplicationScoped
+public class PushBean implements Serializable {
+
+    private static final Logger LOG = Logger.getLogger(PushBean.class.getName());
+
+    @Inject
+    @Push(channel ="clock")
+    private PushContext push;
+
+    public void clockAction() {
+        Calendar now = Calendar.getInstance();
+
+        String time = now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND);
+        LOG.log(Level.INFO, "Time: {0}", time);
+
+        push.send(time);
+    }
+
+}
