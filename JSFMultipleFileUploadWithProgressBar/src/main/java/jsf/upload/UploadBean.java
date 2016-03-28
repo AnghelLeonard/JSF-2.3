26e84c07123b086ce5ac2ad766369faccaf352b4
AnghelLeonard <leoprivacy@yahoo.com>
2016-03-28 16:48:08 +0300

    File upload with AJAX and progress bar implemented via web sockets

--- JSFMultipleFileUploadWithProgressBar/src/main/java/jsf/upload/UploadBean.java
+++ JSFMultipleFileUploadWithProgressBar/src/main/java/jsf/upload/UploadBean.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.enterprise.context.SessionScoped;
+import javax.faces.view.ViewScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -19,7 +19,7 @@ import javax.inject.Named;
 import javax.servlet.http.Part;
 
 @Named
-@SessionScoped
+@ViewScoped
 public class UploadBean implements Serializable {
 
     @Inject
